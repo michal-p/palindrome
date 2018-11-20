abd775e4539bd8f97a148d0df9f6d5bc0b7fd0c9
lionel <puchy.michal@gmail.com>
2018-11-20 23:07:51 +0100

    Readme file added

--- index.js
+++ index.js
@@ -1,59 +1,21 @@
-// Reverse string
-String.prototype.reverse = function () {
+module.exports = Phrase;
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
 }
 
-function emailParts(email) {
-  let processedContent = email.toLowerCase();
-  return processedContent.split('@');
-}
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
 
-function Phrase(sentense) {
-  this.content = sentense;
-
-  this.processedContent = function() {
-    return this.processor(this.content);
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
   }
 
-  this.processor = function (string) {
-    return string.toLowerCase();
-  }
-
-  // Returns true for the palindrome otherwise return false
+  // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    
     return this.processedContent() === this.processedContent().reverse();
   }
-
-  this.louder = function () {
-    return this.content.toUpperCase();
-  }
-}
-
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-
-  //Override inherited function
-  this.processedContent = function() {
-    return this.processor(this.translation);
-  }
-}
-
-// Inheritance TranslatedPhrase inherit from Phrase
-TranslatedPhrase.prototype = new Phrase();
-
-String.prototype.palindrome = function () {
-  return new Phrase(this).palindrome();
-}
-
-// Blank method
-String.prototype.isBlank = function () {
-  let regexblank = new RegExp("^\\s*$");
-  return !!regexblank.exec(this);
-}
-
-// Last member of an Array
-Array.prototype.getLast = function () {
-  return this.slice(-1);
 }
