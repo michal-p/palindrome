584b2f9b1e83db13f8dc1d645cd0b49305ec1d12
lionel <puchy.michal@gmail.com>
2018-11-27 08:54:47 +0100

    Phrase palindrome test

--- index.js
+++ index.js
@@ -11,11 +11,23 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
+  }
+
+  //Returns true for a palindrome, false otherwise
+  this.letters = function () {
+    // let theLetters = [];
+    // const regexLetters = /[a-zA-Z]/i;
+    // Array.from(this.content).forEach(char => {
+    //   if (char.match(regexLetters)) {
+    //     theLetters.push(char);
+    //   }      
+    // });
+    return Array.from(this.content).filter(c => c.match(/[a-zA-Z]/i)).join("");
   }
 }
---
+++ test/test.js
@@ -0,0 +1,32 @@
+let assert = require("assert");
+let Phrase = require("../index.js");
+
+describe("Phrase", function () {
+  describe("#palindrome", function () {
+    it("Should return false for a non-palindrome.", function () {
+      let nopalindrome = new Phrase("apple");
+      assert(!nopalindrome.palindrome());
+    })
+
+    it("should return true for a plain palindrome", function() {
+      let plainPalindrome = new Phrase("racecar");
+      assert(plainPalindrome.palindrome());
+    });
+
+    it("should return true for a mixed-case palindrome", function () {
+      let mixedCasePalindrome = new Phrase("RaceCar");
+      assert(mixedCasePalindrome.palindrome());
+    });
+
+    it("should return true for a palindrome with punctuation", function () {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert(punctuatedPalindrome.palindrome());
+    });
+  })
+  describe("#letters", function () {
+    it("should return only letters", function () {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert.strictEqual("MadamImAdam", punctuatedPalindrome.letters());
+    })
+  })
+})
