2cfa7ee702b3c37ba49d668c8efe265cc226a720
lionel <puchy.michal@gmail.com>
2019-05-20 23:40:50 +0200

    Extend test of #letters

--- test/test.js
+++ test/test.js
@@ -22,11 +22,16 @@ describe("Phrase", function () {
       let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
       assert(punctuatedPalindrome.palindrome());
     });
-  })
+  });
   describe("#letters", function () {
     it("should return only letters", function () {
       let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
       assert.strictEqual("MadamImAdam", punctuatedPalindrome.letters());
-    })
-  })
-})
+    });
+
+    it("should return the empty string on no match", function () {
+      let noLetters = new Phrase("1234.56");
+      assert.strictEqual(noLetters.letters(), "");
+    });
+  });
+});
